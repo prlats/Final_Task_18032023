6dec5cf1489ef8cc67973f058bea9958b55bcf63
prlats <latsples@gmail.com>
2023-03-18 20:09:30 +0300

    Заполнение первого массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿using System;
+using static System.Console;
+Write ("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
 
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(ReadLine());
+    arrayStrings[i] = element;
+}
